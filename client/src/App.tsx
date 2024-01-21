b562f48e6c915e7ca59fd81228798d4bda326d25
code322 <gere.dev@protonmain.com>
2024-01-20 21:21:00 -0500

    fetch the products

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,26 @@
 import { useEffect, useState } from "react";
 
 import "./App.css";
+import { Products } from "./products";
 
 function App() {
+  const [products, setProducts] = useState<Products[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5027/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
 
 
-  return <div>store</div>;
+
+
+
+  return <div>{
+    products.map((product) => (
+      <li key={product.id}>{product.name}</li>
+    ))
+  }</div>;
 }
 
 export default App;
---
+++ client/src/products.ts
@@ -0,0 +1,10 @@
+export interface Products {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    pictureUrl: string;
+    brand: string;
+    type: string;
+    quantityInStock: number;
+}
