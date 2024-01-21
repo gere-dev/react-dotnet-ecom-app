9cb718ed8e160676c00aa72a5f56ff571ff87a51
code322 <gere.dev@protonmain.com>
2024-01-21 18:18:41 -0500

    handle interceptor error

--- client/src/pages/about/About.tsx
+++ client/src/pages/about/About.tsx
@@ -2,35 +2,50 @@ import React from "react";
 import agent from "../../api/agent";
 
 const About = () => {
+  window.addEventListener("unhandledrejection", (event) => {
+    console.error("Uncaught Promise Rejection:", event.reason);
+  });
   return (
     <div className="max-width mx-auto px-3 flex gap-3">
       <button
         className="bg-blue-500 text-white"
-        onClick={() => agent.TestError.get400Error()}
+        onClick={() =>
+          agent.TestError.get400Error().catch((error) => console.log(error))
+        }
       >
         Test 400 Error
       </button>
       <button
         className="bg-blue-500 text-white"
-        onClick={() => agent.TestError.get401Error()}
+        onClick={() =>
+          agent.TestError.get401Error().catch((error) => console.log(error))
+        }
       >
         Test 401 Error
       </button>
       <button
         className="bg-blue-500 text-white"
-        onClick={() => agent.TestError.get404Error()}
+        onClick={() =>
+          agent.TestError.get404Error().catch((error) => console.log(error))
+        }
       >
         Test 404 Error
       </button>
       <button
         className="bg-blue-500 text-white"
-        onClick={() => agent.TestError.get500Error()}
+        onClick={() =>
+          agent.TestError.get500Error().catch((error) => console.log(error))
+        }
       >
         Test 500 Error
       </button>
       <button
         className="bg-blue-500 text-white"
-        onClick={() => agent.TestError.getValidationError()}
+        onClick={() =>
+          agent.TestError.getValidationError().catch((error) =>
+            console.log(error)
+          )
+        }
       >
         Test Validation Error
       </button>
