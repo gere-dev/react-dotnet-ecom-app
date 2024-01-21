1d9be77fff1d89607c30465d0d59cb35bdb5125f
code322 <gere.dev@protonmain.com>
2024-01-21 00:00:09 -0500

    install browser router and set it up;

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
+import { Route, Routes, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import Catalog from "./components/catalog/Catalog";
 import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      <Catalog />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ client/src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Catalog from "../../components/catalog/Catalog";
+
+const Home = () => {
+  return (
+    <>
+      <Catalog />
+    </>
+  );
+};
+
+export default Home;
