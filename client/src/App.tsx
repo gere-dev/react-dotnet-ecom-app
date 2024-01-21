d59a3b0f39b38bb115c2fe17fb7b06ed06a1890a
code322 <gere.dev@protonmain.com>
2024-01-21 00:03:39 -0500

    remove unused code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
-import { Route, Routes, createBrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Catalog from "./components/catalog/Catalog";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
