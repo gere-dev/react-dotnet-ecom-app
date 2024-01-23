4b8cbc779660204a2557308a39796a0ea38181a5
code322 <gere.dev@protonmain.com>
2024-01-23 10:06:44 -0500

    create basket controller

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public BasketController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
+            var basket = await _context.Baskets
+            .Include(i => i.Items)
+            .ThenInclude(p => p.Product)
+            .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["BuyerId"]);
+
+            if (basket == null) return NotFound();
+
+            return basket;
+            ;
+        }
+    }
+}
