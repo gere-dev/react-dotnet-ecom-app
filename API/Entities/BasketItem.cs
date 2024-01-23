a15f859947042bb97f687146c0b9ad98564fd482
code322 <gere.dev@protonmain.com>
2024-01-23 09:42:39 -0500

    add the basketsItem to migrations

--- API/Entities/BasketItem.cs
+++ API/Entities/BasketItem.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities
 {
+
+    [Table("BasketItems")] //table name will be BasketItems instead of BasketItem(as referred in the class)
     public class BasketItem
     {
         public int Id { get; set; }
         public int Quantity { get; set; }
+
+        // navigation properties
         public int ProductId { get; set; }
         public Product Product { get; set; }
 
-
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }
     }
 }
