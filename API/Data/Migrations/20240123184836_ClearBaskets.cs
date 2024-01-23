ccc0b0b73419a69c3fc7e6be1805f6c94c4ca066
code322 <gere.dev@protonmain.com>
2024-01-23 13:53:40 -0500

    replace the bastket reponse with BasketDtos

---
+++ API/Data/Migrations/20240123184836_ClearBaskets.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClearBaskets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Baskets;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
