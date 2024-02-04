b99df7623605afe131e64b5c0ea01004a8201be9
code322 <gere.dev@protonmain.com>
2024-02-04 14:55:29 -0500

    change the basket path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,25 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Header from "./components/header/Header";
-import Home from "./pages/home/Home";
-import Catalog from "./pages/catalog/Catalog";
-import About from "./pages/about/About";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import ServerError from "./pages/errors/ServerError";
-import Basket from "./pages/basket/Basket";
-import { useStoreContext } from "./contexts/StoreContext";
-import { useEffect, useState } from "react";
-import { getCookie } from "./utils/utils";
-import agent from "./api/agent";
-import Loading from "./components/Loading";
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import Catalog from './pages/catalog/Catalog';
+import About from './pages/about/About';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ServerError from './pages/errors/ServerError';
+import Basket from './pages/Basket';
+import { useStoreContext } from './contexts/StoreContext';
+import { useEffect, useState } from 'react';
+import { getCookie } from './utils/utils';
+import agent from './api/agent';
+import Loading from './components/Loading';
 
 function App() {
   const { setBasket } = useStoreContext();
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const buyerId = getCookie("buyerId");
+    const buyerId = getCookie('buyerId');
     if (buyerId) {
       agent.Basket.get()
         .then((basket) => setBasket(basket))
@@ -37,13 +37,13 @@ function App() {
   return (
     <>
       <Header />
-      <ToastContainer position="bottom-right" toastClassName="dark-toast" autoClose={2000} />
+      <ToastContainer position='bottom-right' toastClassName='dark-toast' autoClose={2000} />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/basket" element={<Basket />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/catalog/:id" element={<Catalog />} />
-        <Route path="/server-error" element={<ServerError />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/basket' element={<Basket />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/catalog/:id' element={<Catalog />} />
+        <Route path='/server-error' element={<ServerError />} />
       </Routes>
     </>
   );
