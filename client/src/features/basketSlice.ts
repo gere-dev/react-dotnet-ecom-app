4ef51efb782f48e68017c098b02eaa4ac329fdb6
code322 <gere.dev@protonmain.com>
2024-02-08 14:34:19 -0500

    create an addBasketItemAsync action

--- client/src/features/basketSlice.ts
+++ client/src/features/basketSlice.ts
@@ -1,14 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { BasketType } from '../types/BasketType';
+import agent from '../api/agent';
 
 interface BasketState {
   basket: BasketType | null;
+  status: 'idle' | 'pending' | 'fulfilled' | 'rejected';
 }
 
 const initialState: BasketState = {
   basket: null,
+  status: 'idle',
 };
 
+export const addBasketItemAsync = createAsyncThunk<BasketType, { productId: number; quantity: number }>(
+  'basket/AddBasketItem',
+  async ({ productId, quantity }) => {
+    try {
+      const response = await agent.Basket.addItem(productId, quantity);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 export const basketSlice = createSlice({
   name: 'basket',
   initialState,
@@ -23,6 +37,19 @@ export const basketSlice = createSlice({
       state.basket!.items[itemIndex].quantity -= quantity;
       if (state.basket?.items[itemIndex].quantity === 0) state.basket.items.splice(itemIndex, 1);
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(addBasketItemAsync.pending, (state, action) => {
+      state.status = 'pending';
+    });
+    builder.addCase(addBasketItemAsync.fulfilled, (state, action) => {
+      state.basket = action.payload;
+      state.status = 'fulfilled';
+    });
+    builder.addCase(addBasketItemAsync.rejected, (state, action) => {
+      // state.basket = action.payload;
+      state.status = 'rejected';
+    });
   },
 });
 
